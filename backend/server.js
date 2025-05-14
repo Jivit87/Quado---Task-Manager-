8e220b1c78e2feef65fb3094eca765421a89c655
Jivit87 <jivitrana87@gmail.com>
2025-05-15 02:26:45 +0530

    Added User backend

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/db");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+// Connecting MongoDB
+connectDB();
+
+// Routes
+const authRoutes = require("./routes/auth");
+
+app.use("/api/auth", authRoutes);
+
+// For error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Something went wrong!" });
+});
+
+app.get("/", (req, res) => {
+  res.send("Task Management API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
